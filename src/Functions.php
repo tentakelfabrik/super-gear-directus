268d6a01b4c507674e4505aa2e4c4f36a5b271a8
Björn <me@tentakelfabrik.de>
2020-06-10 09:43:35 +0200

    adding #4

--- src/Functions.php
+++ src/Functions.php
@@ -10,3 +10,33 @@ function app($class)
 {
     return new $class();
 }
+
+/**
+ *  function similar to blade asset
+ *
+ *  @param $path
+ *  @return string
+ *
+ */
+function asset($path, $prefix = '/public')
+{
+    // get flight
+    $app = Flight::app();
+
+    // getting basePath
+    $basePath = $app->get('basePath');
+
+    // path to mix-manifest
+    $file = $app->get('basePath').'mix-manifest.json';
+
+    if (file_exists($file)) {
+        $manifest = file_get_contents($file);
+        $files = json_decode($manifest, true);
+
+        if (isset($files[$prefix.$path])) {
+            $path = str_replace($prefix, '', $files[$prefix.$path]);
+        }
+    }
+
+    return $path;
+}
